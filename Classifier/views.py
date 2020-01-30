141b5f723022dde8265a30b6d68bc90f000bbc01
Abdullah <abdullamajid45@gmail.com>
2020-01-30 16:45:42 +0500

    testing by prining all text

--- Classifier/views.py
+++ Classifier/views.py
@@ -61,35 +61,39 @@ def index(request):
     return render(request,'index.html')
 
 def output(request):
-    # file = request.FILES["file"]
-    # fs = FileSystemStorage()
-    # name = fs.save(file.name, file)
-    #
-    # try:
-    #     data = pdf_to_text(str(name))
-    #
-    #     data['text'] = [entry.lower() for entry in data['text']]
-    #     data['text'] = [word_tokenize(entry) for entry in data['text']]
-    #
-    #     tag_map = defaultdict(lambda: wn.NOUN)
-    #     tag_map['J'] = wn.ADJ
-    #     tag_map['V'] = wn.VERB
-    #     tag_map['R'] = wn.ADV
-    #
-    #     for index, entry in enumerate(data['text']):
-    #         # Declaring Empty List to store the words that follow the rules for this step
-    #         Final_words = []
-    #         # Initializing WordNetLemmatizer()
-    #         word_Lemmatized = WordNetLemmatizer()
-    #         # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
-    #         for word, tag in pos_tag(entry):
-    #             # Below condition is to check for Stop words and consider only alphabets
-    #             if word not in stopwords.words('english') and word.isalpha():
-    #                 word_Final = word_Lemmatized.lemmatize(word, tag_map[tag[0]])
-    #                 Final_words.append(word_Final)
-    #         data['text'][index] = Final_words
-    #     data["text"] = [" ".join(entry) for entry in data['text']]
-    #     x = data["text"].copy()
+    file = request.FILES["file"]
+    fs = FileSystemStorage()
+    name = fs.save(file.name, file)
+
+    try:
+        data = pdf_to_text(str(name))
+
+        data['text'] = [entry.lower() for entry in data['text']]
+        data['text'] = [word_tokenize(entry) for entry in data['text']]
+
+        tag_map = defaultdict(lambda: wn.NOUN)
+        tag_map['J'] = wn.ADJ
+        tag_map['V'] = wn.VERB
+        tag_map['R'] = wn.ADV
+
+        for index, entry in enumerate(data['text']):
+            # Declaring Empty List to store the words that follow the rules for this step
+            Final_words = []
+            # Initializing WordNetLemmatizer()
+            word_Lemmatized = WordNetLemmatizer()
+            # pos_tag function below will provide the 'tag' i.e if the word is Noun(N) or Verb(V) or something else.
+            for word, tag in pos_tag(entry):
+                # Below condition is to check for Stop words and consider only alphabets
+                if word not in stopwords.words('english') and word.isalpha():
+                    word_Final = word_Lemmatized.lemmatize(word, tag_map[tag[0]])
+                    Final_words.append(word_Final)
+            data['text'][index] = Final_words
+        data["text"] = [" ".join(entry) for entry in data['text']]
+        x = data["text"].copy()
+        result=[]
+        for item in x:
+            result.append(item)
+
     #     train_x = Count_vect.transform(x)
     #     y_pred = []
     #     if train_x.shape[0] > 0:
@@ -100,12 +104,9 @@ def output(request):
     #             result.append(data["page"][i])
     #     print("Result=========== ", result)
     #
-    # except:
-    #     result=[]
-    # fs.delete(name)
-    result=[]
-    result.append(str(type(classifier_svc)))
-    result.append(str(type(Count_vect)))
-    result.append(str(type(Encoder)))
+    except:
+        result=[]
+    fs.delete(name)
+
     final = {"data": result}
     return render(request, 'index.html', final)
