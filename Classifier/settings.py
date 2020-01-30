eecddfd1f54c2f9d7b9e18e2a70b99fe5db3a69c
Abdullah <abdullamajid45@gmail.com>
2020-01-30 12:57:00 +0500

    hosts allowed

--- Classifier/settings.py
+++ Classifier/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ye4)anz#!ivpdmc5c*)0o=gvx&pt%gbrie5_+)z%=4c38((c(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
