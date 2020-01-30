517fb2db5a72811b4b70083aa8fb840a7f178161
Abdullah <abdullamajid45@gmail.com>
2020-01-30 16:13:22 +0500

    Procfile extension removed + wsgi updated

--- Classifier/wsgi.py
+++ Classifier/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classifier.Classifier.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Classifier.settings')
 
 application = get_wsgi_application()
