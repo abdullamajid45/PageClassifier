0c11e4893869419984096e85b0c3a66308e62bd1
Abdullah <abdullamajid45@gmail.com>
2020-01-31 09:52:03 +0500

    testing withou pos-tag and lemmatizer

--- Classifier/views.py
+++ Classifier/views.py
@@ -64,7 +64,7 @@ def output(request):
     file = request.FILES["file"]
     fs = FileSystemStorage()
     name = fs.save(file.name, file)
-
+    result=[]
     try:
         data = pdf_to_text(str(name))
 
@@ -74,7 +74,7 @@ def output(request):
         # tag_map['J'] = wn.ADJ
         # tag_map['V'] = wn.VERB
         # tag_map['R'] = wn.ADV
-
+        result.append("Hello")
         index=0
         for entry in data['text']:
             # Declaring Empty List to store the words that follow the rules for this step
@@ -87,6 +87,7 @@ def output(request):
             #     if word not in stopwords.words('english') and word.isalpha():
             #         word_Final = word_Lemmatized.lemmatize(word, tag_map[tag[0]])
             #         Final_words.append(word_Final)
+            result.append("Hello-"+str(index))
             for word in entry:
                 # Below condition is to check for Stop words and consider only alphabets
                 if word not in stopwords.words('english') and word.isalpha():
@@ -94,12 +95,14 @@ def output(request):
 
             data['text'][index] = Final_words
             index+=1
-
+        result.append("World")
         data["text"] = [" ".join(entry) for entry in data['text']]
         x = data["text"].copy()
+        result.append("World-1")
         train_x = Count_vect.transform(x)
         y_pred = []
         if train_x.shape[0] > 0:
+            result.append("World-2")
             y_pred = classifier_svc.predict(train_x)
         result = []
         for i in range(0, len(y_pred)):
@@ -108,7 +111,9 @@ def output(request):
         print("Result=========== ", result)
 
     except:
-        result=[]
+        print("")
+        #result=[]
+    result.append("World-4")
     fs.delete(name)
     final = {"data": result}
     return render(request, 'index.html', final)
